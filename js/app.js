3805c9e7a8153a427bab17cc478764fa8f765ab9
dekoy4494 <yoked1994@gmail.com>
2020-01-20 15:33:52 +0200

    Adding footer and some changes in css

--- js/app.js
+++ js/app.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
 
-    $('.gs-slider').slick({
-        slidesToShow: 1,
+	$('.gs-slider').slick({
+		slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: true,
 		autoplay: false,
-		dots:true,
-    });
+		dots: true,
+	});
+
+});
+
+$(document).ready(function () {
+
+	$('.gs-partners__slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		dots: false,
+		autoplaySpeed: 500,
+		pauseOnFocus: true,
+	
+
+	});
 });
