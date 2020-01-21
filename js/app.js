f1e1cadf75708753e0cae3c9208274e7835b8224
dekoy4494 <yoked1994@gmail.com>
2020-01-21 22:16:00 +0200

    adding hover,animated and wow efects

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
+"use strict";
+
+//wow.animate
+new WOW().init(); 
+
 $(document).ready(function () {
 
+//slider
 	$('.gs-slider').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
@@ -9,7 +15,7 @@ $(document).ready(function () {
 	});
 
 });
-
+//another slider
 $(document).ready(function () {
 
 	$('.gs-partners__slider').slick({
@@ -23,4 +29,64 @@ $(document).ready(function () {
 	
 
 	});
+});
+
+//hover effect logo//
+const text = 'KALLYAS';
+
+// this function turns a string into an array
+const createLetterArray = (string) => {
+  return string.split('');
+}
+
+// this function creates letter layers wrapped in span tags
+const createLetterLayers = (array) => {
+  return array.map((letter) => {
+      let layer = '';
+      //specify # of layers per letter
+      for (let i = 1; i <= 2; i++) {
+        // if letter is a space
+        if(letter == ' '){
+          layer += '<span class="space"></span>';
+        }else{
+          layer += '<span class="letter-'+i+'">'+letter+'</span>';
+        }
+      }
+      return layer;
+  });
+}
+
+// this function wraps each letter in a parent container
+const createLetterContainers = (array) => {
+  return array.map((item) => {
+    let container = '';
+    container += '<div class="wrapper">'+item+'</div>';
+    return container;
+  });
+}
+
+// use a promise to output text layers into DOM first
+const outputLayers = new Promise(function(resolve, reject) {
+      document.getElementById('text').innerHTML = createLetterContainers(createLetterLayers(createLetterArray(text))).join('');
+      resolve();
+});
+
+// then adjust width and height of each letter
+const spans = Array.prototype.slice.call(document.getElementsByTagName('span'));
+outputLayers.then(() => {
+    return spans.map((span) => {
+      setTimeout(() => {
+        span.parentElement.style.width = span.offsetWidth+'px';
+        span.parentElement.style.height = span.offsetHeight+'px';
+      }, 250);
+    });  
+}).then(() => {
+    // then slide letters into view one at a time
+    let time = 250;
+    return spans.map((span) => {
+      time += 75;
+      setTimeout(() => {
+        span.parentElement.style.top = '100px';
+      }, time);
+    });
 });
