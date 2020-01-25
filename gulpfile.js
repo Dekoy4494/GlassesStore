c886791bce1c0c9cd75ac7c4390966e8d3df9210
dekoy4494 <yoked1994@gmail.com>
2020-01-26 01:33:10 +0200

    fixed slider autoplay in banners section, add mobile response

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,42 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- 
+const browserSync = require('browser-sync').create();
+
+
+function sync(cb) {
+  let baseDir = './GlassesStore/**/*.*';
+  browserSync.init({
+    server: {
+      baseDir
+    },
+    port: 3001
+  })
+  cb();
+}
+
+function reload(cb) {
+
+  browserSync.reload();
+
+  cb();
+}
+
+gulp.task('sync', sync);
+
+
 sass.compiler = require('node-sass');
- 
+
 gulp.task('scss', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./styles'));
 });
- 
+
 gulp.task('scss:watch', function () {
   gulp.watch('./scss/**/*.scss', gulp.parallel('scss'));
 });
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,44 +1,48 @@
 "use strict";
 
 //wow.animate
-new WOW().init(); 
+new WOW().init();
 
 $(document).ready(function () {
 
-//slider
-	$('.gs-slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: true,
-		autoplay: false,
-		dots: true,
-	});
+  //slider
+  $('.gs-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: false,
+    dots: true,
+  });
 
 });
 
 $(document).ready(function () {
-//another slider
-	$('.gs-partners__slider').slick({
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		arrows: false,
-		autoplay: true,
-		dots: false,
-		autoplaySpeed: 500,
-		pauseOnFocus: true,
+  //another slider
+  $('.gs-partners__slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    dots: false,
+    autoplaySpeed: 0,
+    pauseOnFocus: true,
+    cssEase: 'linear',
+    
   });
   //search-click
-  $("#showSearch").on('click', function() {
+  $("#showSearch").on('click', function () {
     $('.gs-search-form').toggleClass('open');
 
-    if('.open') {
-        $('#showSearch i').toggleClass('fas fa-search')
-        $('#showSearch i').toggleClass('fas fa-times')
+    if ('.open') {
+      $('#showSearch i').toggleClass('fas fa-search')
+      $('#showSearch i').toggleClass('fas fa-times')
     }
+  });
+  //parallax
+  $('.parallax-window').parallax({ imageSrc: './img/footer/banner.jpg' });
 });
-//parallax
-$('.parallax-window').parallax({imageSrc: './img/footer/banner.jpg'});
-});
+
+
 
 //hover effect logo//
 const text = 'KALLYAS';
@@ -51,17 +55,17 @@ const createLetterArray = (string) => {
 // this function creates letter layers wrapped in span tags
 const createLetterLayers = (array) => {
   return array.map((letter) => {
-      let layer = '';
-      //specify # of layers per letter
-      for (let i = 1; i <= 2; i++) {
-        // if letter is a space
-        if(letter == ' '){
-          layer += '<span class="space"></span>';
-        }else{
-          layer += '<span class="letter-'+i+'">'+letter+'</span>';
-        }
+    let layer = '';
+    //specify # of layers per letter
+    for (let i = 1; i <= 2; i++) {
+      // if letter is a space
+      if (letter == ' ') {
+        layer += '<span class="space"></span>';
+      } else {
+        layer += '<span class="letter-' + i + '">' + letter + '</span>';
       }
-      return layer;
+    }
+    return layer;
   });
 }
 
@@ -69,45 +73,61 @@ const createLetterLayers = (array) => {
 const createLetterContainers = (array) => {
   return array.map((item) => {
     let container = '';
-    container += '<div class="wrapper">'+item+'</div>';
+    container += '<div class="wrapper">' + item + '</div>';
     return container;
   });
 }
 
 // use a promise to output text layers into DOM first
-const outputLayers = new Promise(function(resolve, reject) {
-      document.getElementById('text').innerHTML = createLetterContainers(createLetterLayers(createLetterArray(text))).join('');
-      resolve();
+const outputLayers = new Promise(function (resolve, reject) {
+  document.getElementById('text').innerHTML = createLetterContainers(createLetterLayers(createLetterArray(text))).join('');
+  resolve();
 });
 
 // then adjust width and height of each letter
 const spans = Array.prototype.slice.call(document.getElementsByTagName('span'));
 outputLayers.then(() => {
-    return spans.map((span) => {
-      setTimeout(() => {
-        span.parentElement.style.width = span.offsetWidth+'px';
-        span.parentElement.style.height = span.offsetHeight+'px';
-      }, 250);
-    });  
+  return spans.map((span) => {
+    setTimeout(() => {
+      span.parentElement.style.width = span.offsetWidth + 'px';
+      span.parentElement.style.height = span.offsetHeight + 'px';
+    }, 250);
+  });
 }).then(() => {
-    // then slide letters into view one at a time
-    let time = 250;
-    return spans.map((span) => {
-      time += 75;
-      setTimeout(() => {
-        span.parentElement.style.top = '100px';
-      }, time);
-    });
+  // then slide letters into view one at a time
+  let time = 250;
+  return spans.map((span) => {
+    time += 75;
+    setTimeout(() => {
+      span.parentElement.style.top = '100px';
+    }, time);
+  });
 });
 
 
 //email validator 
 
-function validate(form_id,email) {
+function validate(form_id, email) {
   var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   var address = document.forms[form_id].elements[email].value;
-  if(reg.test(address) == false) {
-     alert('Enter correct e-mail');
-     return false;
+  if (reg.test(address) == false) {
+    alert('Enter correct e-mail');
+    return false;
   }
+}
+
+
+//ajax
+
+let ajax = new XMLHttpRequest();
+
+ajax.onreadystatechange = function () {
+  if (ajax.readyState === 4) {
+    document.getElementById('responce').innerHTML = ajax.responseText;
+  }
+}
+
+ajax.open('GET', 'arrivals.html');
+function loadMore () {
+ajax.send();
 }
